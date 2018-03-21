d6e5ca3618015ba1cff343a2699e1eeb4a595158
Ahmed Tadde <ahmedtadde@gmail.com>
2018-03-21 18:46:53 -0400

    light edit of project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const gulp = require('gulp');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
+const plumber = require('gulp-plumber');
 const autoprefixer = require('gulp-autoprefixer');
 const uglifycss = require('gulp-uglifycss');
 const babel = require('gulp-babel');
 const uglyfly = require('gulp-uglyfly');
 
-gulp.task('default', () => {
-    gulp.src('./src/public/styles.css')
+
+gulp.task('minify-css-files', () => {
+    gulp.src('./src/client/styles.css')
+        .pipe(plumber())
         .pipe(autoprefixer({
             browsers: ['last 50 versions'],
             cascade: false
@@ -17,8 +20,11 @@ gulp.task('default', () => {
           }))
         .pipe(rename('styles.min.css'))
         .pipe(gulp.dest('./dist/'));
+});
 
-    gulp.src('src/public/index.js')
+gulp.task('minify-js-scripts', () => {
+    gulp.src('src/client/index.js')
+        .pipe(plumber())
         .pipe(babel({
             presets: ['env']
         }))
@@ -26,3 +32,10 @@ gulp.task('default', () => {
         .pipe(rename('index.min.js'))
         .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('watch', () => {
+    gulp.watch('./src/client/styles.css', ['minify-css-files']);
+    gulp.watch('./src/client/index.js', ['minify-js-scripts']);
+});
+
+gulp.task('default', ['minify-css-files','minify-js-scripts', 'watch']);
